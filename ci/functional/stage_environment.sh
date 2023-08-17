eebc8d7167798dfb9b88fa24305e04d595d05db0
Terry McGuinness <terry.mcguinness@noaa.gov>
2023-08-17 15:39:35 -0500

    small advance on stage_enviroment that clones a PR into functional setup env

--- ci/functional/stage_environment.sh
+++ ci/functional/stage_environment.sh
@@ -1,7 +1,12 @@
-
+#!/bin/bash
+#set -eux
 ################################################################
 # Setup the reletive paths to scripts and PS4 for better logging 
 ################################################################
+
+export GH=${HOME}/bin/gh
+export REPO_URL=${REPO_URL:-"https://github.com/NOAA-EMC/global-workflow.git"}
+
 HOMEgfs="$(cd "$(dirname  "${BASH_SOURCE[0]}")/../.." >/dev/null 2>&1 && pwd )"
 echo "HOMEgfs: ${HOMEgfs}"
 scriptname=$(basename "${BASH_SOURCE[0]}")
@@ -16,7 +21,8 @@ source "${HOMEgfs}/ush/detect_machine.sh"
 case ${MACHINE_ID} in
   hera | orion)
     echo "Setting up environment for running Fuctional Based Automated Testing on ${MACHINE_ID}"
-    source "${HOMEgfs}/workflow/gw_setup.sh"
+    #source "${HOMEgfs}/workflow/gw_setup.sh"
+    source "${HOMEgfs}/ci/platforms/${MACHINE_ID}.sh"
     ;;
   *)
     echo "Unsupported platform. Exiting with error."
@@ -24,3 +30,13 @@ case ${MACHINE_ID} in
     ;;
 esac
 
+pr_list=$(${GH} pr list --repo "${REPO_URL}" --label "CI-${MACHINE_ID^}-Functiontest-Ready" --state "open" | awk '{print $1}') || true
+for pr in ${pr_list}; do
+ echo "PRs ready: ${pr}"
+done
+
+# TODO Hard code a specific PR number for now and 
+# develop the rest of the functional test infrastructure
+PR="${PR_FUNCTIONAL_TEST}"
+
+"${HOMEgfs}/ci/scripts/clone-build_ci.sh -p ${PR} -d ${FUNCTESTS_DATA_ROOT}"
