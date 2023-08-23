2b48b0312cf40db4ba34d7c4ae8353a673cc49a7
Terry.McGuinness <Terry.McGuinness@noaa.gov>
2023-08-23 21:59:29 +0000

    more pycodestyle focusted on spaceing

--- ci/functional/test_configuration.py
+++ ci/functional/test_configuration.py
@@ -28,10 +28,10 @@ def input_args():
         """
 
     parser = ArgumentParser(description=description,
-                            formatter_class=ArgumentDefaultsHelpFormatter)
+                            formatter_class = ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('expdir', help='full path to experiment directory containing config files',
-                        type=str, default=os.environ['PWD'])
+                        type=str, default = os.environ['PWD'])
 
     args = parser.parse_args()
 
@@ -39,11 +39,13 @@ def input_args():
 
 if __name__ == '__main__':
 
+
     user_inputs = input_args()
     cfg = Configuration(user_inputs.expdir)
 
     list_files = list(map(path.basename, cfg.config_files))
-    col = 5; l = len(list_files)
+    col = 5
+    l = len(list_files)
     list_files.extend([' ']*(col*ceil(l/col)-l))
     config_files = [list_files[i:i+col] for i in range(0, l, col)]
     table=columnar(config_files, headers = None, no_borders = True)
@@ -67,13 +69,12 @@ if __name__ == '__main__':
     print('CASE:\t\t', base['CASE'])
     print('RUN:\t\t', base['RUN'])
 
-
     if 'config.anal' in '\n'.join(cfg.config_files):
-       print('\nconfig.anal...configured: ',end = '')
-       #cfg.print_config(['config.base', 'config.anal'])
-       anal = cfg.parse_config(['config.base', 'config.anal'])
-       print("DO_ATM",anal['DO_ATM'])
-       print('')
+        print('\nconfig.anal...configured: ',end = '')
+        #cfg.print_config(['config.base', 'config.anal'])
+        anal = cfg.parse_config(['config.base', 'config.anal'])
+        print("DO_ATM", anal['DO_ATM'])
+        print('')
 
 
     if 'config.coupled_ic' in '\n'.join(cfg.config_files):
