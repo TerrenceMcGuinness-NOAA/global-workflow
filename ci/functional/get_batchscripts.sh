a696cfa48a32f7ce13c0faf2be7c07da149446f1
Terry.McGuinness <Terry.McGuinness@noaa.gov>
2023-08-23 21:02:23 +0000

    fixed shellcheck errors

--- ci/functional/get_batchscripts.sh
+++ ci/functional/get_batchscripts.sh
@@ -2,23 +2,24 @@
 
 EXPDIR=$1
 SDATE=$2
-PSLOT=$(basename $EXPDIR)
+PSLOT=$(basename "${EXPDIR}")
 
 rocotorun -v 10 -d "${EXPDIR}/${PSLOT}.db" -w "${EXPDIR}${PSLOT}.xml" -c "${SDATE}00" -t gfsfcst 2>/dev/null
-list_jobs=$(rocotostat -v 10 -d $EXPDIR/${PSLOT}.db -w $EXPDIR/${PSLOT}.xml | grep -Ev 'CYCLE|===' | awk '{print $2}')
+list_jobs=$(rocotostat -v 10 -d "${EXPDIR}/${PSLOT}.db" -w "${EXPDIR}/${PSLOT}.xml" | grep -Ev 'CYCLE|===' | awk '{print $2}' || true)
 
 regex="\{\{(.*)\}\}"
 
-echo "Full job list: $list_jobs"
+echo "Full job list: ${list_jobs}"
 list_jobs="gfsfcst gfsvrfy gfsarch"
-echo "Createing batch scripts for: $list_jobs"
+echo "Createing batch scripts for: ${list_jobs}"
 
 for job in ${list_jobs}; do
   echo $job
   rm -f /tmp/temp.txt
-  yes | rocotoboot -v 10 -d $EXPDIR/${PSLOT}.db -w $EXPDIR/${PSLOT}.xml -c ${SDATE}00 -t $job >& /tmp/temp.txt
+  rocotoboot -v 10 -d "${EXPDIR}/${PSLOT}.db" -w "${EXPDIR}/${PSLOT}.xml" -c "${SDATE}00" -t $job >& /tmp/temp.txt || true
   sub_script=$(cat /tmp/temp.txt)
-  rocotorewind -d $EXPDIR/${PSLOT}.db -w $EXPDIR/${PSLOT}.xml -c ${SDATE}00 -t $job
+  echo "'rocotorewind -d ${EXPDIR}/${PSLOT}.db -w ${EXPDIR}/${PSLOT}.xml -c ${SDATE}00 -t ${job}"
+  rocotorewind -d "${EXPDIR}/${PSLOT}.db" -w "${EXPDIR}/${PSLOT}.xml" -c "${SDATE}00" -t "${job}"
   if [[ $sub_script =~ $regex ]]; then
      echo "${BASH_REMATCH[1]}" | tr -s '\n' > ${job}_${PSLOT}.sub
   fi
